20d81303cf495517f9400e4a6669b3d2ed103747
Szilard95 <dom.szilard@gmail.com>
2012-09-09 22:51:45 +0200

    new explosionRadius

--- entities/blocks/Barrel.lua
+++ entities/blocks/Barrel.lua
@@ -9,21 +9,18 @@ local img = "/res/Barrel.png"
 Barrel = class('Barrel', Block )
 
 function Barrel:initialize(x,y,...)
-	self.scale = 0.5
-	self.explosionRadius = 25
+	--self.scale = 0.5
+	self.explosionRadius = 40
 	self.explosionDamage = 30
 	if #arg == 2 then Block.initialize(self,x,y,arg[1],arg[2])
 	else
 		Block.initialize(self,x,y,"/res/Barrel.png",1)
 	end
-	THIS = self.w
 end
 
 function Barrel:damage(d,owner)
-  if not self.hp then return end
-  self.hp = self.hp - d
   self.owner = owner
-  if self.hp <= 0 then self:explode(self.explosionRadius,self.explosionDamage) end
+  self:explode(self.explosionRadius,self.explosionDamage)
 end
 
 
