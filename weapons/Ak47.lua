c990b22fe767635501eac0cebd689933c33cc906
Szilard95 <dom.szilard@gmail.com>
2012-09-09 17:25:19 +0200

    added maxAmmo

--- weapons/Ak47.lua
+++ weapons/Ak47.lua
@@ -5,12 +5,13 @@
 local  Weapon = require "weapons.Weapon"
 local dmg = 9
 local rate = 10
-local ammo = 25
+local ammo = 30
+local maxAmmo = ammo*2
 local range = 370
 Ak47 = class('Ak47', Weapon)
 
 function Ak47:initialize()
-	Weapon.initialize(self,rate,dmg,ammo,range)
+	Weapon.initialize(self,rate,dmg,ammo,range,maxAmmo)
 	self.name = 'ak47'
 end
 
--- weapons/Smg.lua
+++ weapons/Smg.lua
@@ -6,11 +6,12 @@ local  Weapon = require "weapons.Weapon"
 local dmg = 5
 local rate = 15
 local ammo = 35
+local maxAmmo = 70
 local range = 300
 Smg = class('Smg', Weapon)
 
 function Smg:initialize()
-	Weapon.initialize(self,rate,dmg,ammo,range)
+	Weapon.initialize(self,rate,dmg,ammo,range,maxAmmo)
 	self.name = "smg"
 end
 
