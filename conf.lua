ffee6601d487757e9f4134528ce77456297c6fbb
Szilard95 <dom.szilard@gmail.com>
2012-09-09 17:21:48 +0200

    optimized

--- conf.lua
+++ conf.lua
@@ -2,4 +2,14 @@ function love.conf(t)
     t.screen.width = 1280
     t.screen.height = 736
     t.screen.vsync = false
+    t.modules.joystick = false   -- Enable the joystick module (boolean)
+    t.modules.audio = true      -- Enable the audio module (boolean)
+    t.modules.keyboard = true   -- Enable the keyboard module (boolean)
+    t.modules.event = true      -- Enable the event module (boolean)
+    t.modules.image = true      -- Enable the image module (boolean)
+    t.modules.graphics = true   -- Enable the graphics module (boolean)
+    t.modules.timer = true      -- Enable the timer module (boolean)
+    t.modules.mouse = false      -- Enable the mouse module (boolean)
+    t.modules.sound = true      -- Enable the sound module (boolean)
+    t.modules.physics = false    -- Enable the physics module (boolean)
 end
